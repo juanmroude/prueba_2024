995afcbb9bb47c7c0c3635ebead4b320e3a9a630
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-07-02 21:57:22 -0300

    Moviendo los mapas a la carpeta views

--- mapas.php
+++
@@ -1,4 +0,0 @@
-
-<h1>Ubicaciones de The Candy Sugar</h1>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d420609.47340046737!2d68.86807005593!3d34.55326393657743!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38d1694c3c1e6d49%3A0xebdf473578214429!2sKabul%2C%20Afganist%C3%A1n!5e0!3m2!1ses-419!2sar!4v1719855736794!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31271.173202178226!2d92.21978878147851!3d11.559266196965636!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30863a35c7c1b4e1%3A0x22f2e4fd1e2aa9c5!2sIsla%20Sentinel%20del%20Norte!5e0!3m2!1ses-419!2sar!4v1719855786768!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
