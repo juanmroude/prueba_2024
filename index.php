60cf3a4301e0bfe7283108f43f1eec2bc79662db
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-07-02 22:22:40 -0300

    añade favicon

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>The Candy Sugar</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="styles/carrousel.css">
+    <link rel="shortcut icon" href="./img/icon.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
